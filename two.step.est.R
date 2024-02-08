ed5a341e04d4bfc1f02d6aac6e060e06128e2e51
laylaparast <parast@austin.utexas.edu>
2024-02-08 13:31:57 -0600

    Add files via upload

--- two.step.est.R
+++ two.step.est.R
@@ -7,27 +7,29 @@
 #' @return
 #'
 #' @examples
-two.step.est <- function(data.control, data.treat, W.grid.expand) {
-  control.nosurr <- subset(data.control, select = -c(S))
-  treat.nosurr <- subset(data.treat, select = -c(S))
-  names(W.grid.expand) <- names(data.control)[3:ncol(data.control)]
+two.step.est <- function(data.control, data.treat, W.grid.expand.function,h.0,h.1,h.3) {
 
-  working.model.control <- lm(Y ~ ., data = control.nosurr)
-  working.model.treat <- lm(Y ~ ., data = treat.nosurr)
+  names(W.grid.expand.function) <- names(data.control)[3:ncol(data.control)]
 
-  data.control$U.hat <- predict(working.model.treat, data.control) - predict(working.model.control, data.control)
-  data.treat$U.hat <- predict(working.model.treat, data.treat) - predict(working.model.control, data.treat)
-  W.grid.expand$U.hat <- predict(working.model.treat, W.grid.expand) - predict(working.model.control, W.grid.expand)
+   working.model.control <- lm(Y ~ ., data = data.control)
+  working.model.treat <- lm(Y ~ ., data = data.treat)
+
+  
+  data.control.pred = data.control
+  data.control.pred$S = mean(c(data.control$S,data.treat$S))
+  data.treat.pred = data.treat
+  data.treat.pred$S = mean(c(data.control$S,data.treat$S))
+  data.control$U.hat <- predict(working.model.treat, newdata= data.control.pred) - predict(working.model.control, newdata= data.control.pred) 
+  data.treat$U.hat <- predict(working.model.treat, newdata= data.treat.pred) - predict(working.model.control, newdata= data.treat.pred)
+  W.grid.expand.function$S = mean(c(data.control$S,data.treat$S))
+ W.grid.expand.function$U.hat <- predict(working.model.treat, newdata= W.grid.expand.function) - predict(working.model.control, newdata= W.grid.expand.function)
+ 
+W.grid.expand.function = W.grid.expand.function[,-which((names(W.grid.expand.function) == "S"))]
 
   # kernel smoothing
-  kernel <- function(x, h) { return(dnorm(x / h) / h) }
-
-  h.0 <- bw.nrd(data.control$U.hat)*length(data.control$U.hat)^(-0.2)
-  h.1 <- bw.nrd(data.treat$U.hat)*length(data.treat$U.hat)^(-0.2)
-  h.2 <- bw.nrd(data.control$S) * length(data.control$S)^(-0.2)
-  h.3 <- bw.nrd(data.treat$S) * length(data.treat$S)^(-0.2)
-
-  get.delta <- function(u) {
+  kernel <- function(x, h) { return(dnorm(x / h)) }
+    
+   get.delta <- function(u) {
     m.1 <- sum(kernel(data.treat$U.hat - u, h.1) * data.treat$Y / sum(kernel(data.treat$U.hat - u, h.1)))
     m.0 <- sum(kernel(data.control$U.hat - u, h.0) * data.control$Y / sum(kernel(data.control$U.hat - u, h.0)))
     return(m.1 - m.0)
@@ -42,10 +44,11 @@ two.step.est <- function(data.control, data.treat, W.grid.expand) {
     }
     if (sum(is.na(mus))!=0) {
       ind = which(is.na(mus))
+      sub.nona = cbind(data.control$S, mus)[-ind,]
       for (i in ind) {
-        mat <- cbind(abs(data.control$S - data.control$S[i]), mus)
-        mmm <- which(mat[, 1] == min(mat[-i, 1]))[1]
-        mus[i] <- mus[mmm]
+        mat <- cbind(abs(sub.nona[,1] - data.control$S[i]), sub.nona[,2])
+        mmm <- which(mat[, 1] == min(mat[, 1]))[1]
+        mus[i] <- sub.nona[mmm,2]
       }
     }
     m.10 <- sum(mus * kernels) / sum(kernels)
@@ -53,11 +56,11 @@ two.step.est <- function(data.control, data.treat, W.grid.expand) {
     return(m.10-m.0)
   }
 
-  my.grid <- W.grid.expand
+  my.grid <- W.grid.expand.function
 
-  my.grid$delta <- unlist(lapply(my.grid$U.hat, get.delta))
-  my.grid$delta.s <- unlist(lapply(my.grid$U.hat, get.delta.s))
-  my.grid$R.s <- 1 - (my.grid$delta.s / my.grid$delta)
+  my.grid$delta.two.step <- unlist(lapply(my.grid$U.hat, get.delta))
+  my.grid$delta.s.two.step <- unlist(lapply(my.grid$U.hat, get.delta.s))
+  my.grid$R.s.two.step <- 1 - (my.grid$delta.s.two.step / my.grid$delta.two.step)
 
   return(my.grid)
 
